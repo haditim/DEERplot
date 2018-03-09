e8cb3cdda667db13f738e970e6a862e59c9bddb2
M. Hadi <hhhadddi@yahoo.com>
2018-03-09 14:04:07 +0100

    Changes for my scimax org-mode implementations. Changed some offset and data removal settings.

--- DEERplot_noGUI.py
+++ DEERplot_noGUI.py
@@ -8,13 +8,16 @@ import time
 import sys
 from matplotlib.backends.backend_pdf import PdfPages
 from matplotlib import style
+from itertools import cycle
+from matplotlib import colors as mcolors
 
 # My favorite styles. You can find all in here: http://tonysyu.github.io/raw_content/matplotlib-style-gallery/gallery.html
 #style.use('ggplot')
 style.use('seaborn-whitegrid')
 #style.use('bmh')
 #style.use('fivethirtyeight')
-defColorArr = ['cyan','red','magenta','blue','green','purple','brown','yellow','navy','black']
+defColorArr = dict(mcolors.BASE_COLORS, **mcolors.CSS4_COLORS)
+defCol = cycle(defColorArr)
 
 class fn():
     def __init__(self, dataFile, folder = '', dataType = ''):
@@ -46,7 +49,7 @@ class fn():
             self.bckg_ax = loadtxt(os.path.join(self.folder, self.bckg_file), unpack=True)
             self.fit_ax = loadtxt(os.path.join(self.folder, self.fit_file), unpack=True)
             self.distr_ax = loadtxt(os.path.join(self.folder, self.distr_file), unpack=True)
-        #self.res_lines = [line.rstrip('\n') for line in open(os.path.join(self.folder, self.res_file))]
+            #self.res_lines = [line.rstrip('\n') for line in open(os.path.join(self.folder, self.res_file))]
 
 def plot(
         deerFolder = '',
@@ -68,6 +71,11 @@ def plot(
         suptitle = '',
         plotFolder = os.path.realpath(__file__),
         rmFitDistr = [],
+        plotExts = ['png'],
+        dateInPlot = False,
+        showPlot = True,
+        timeTraceXlim = [],
+        fitXlim = []
 ):
     #Handling Offset for '3plotsWoffset'
     maxDistrInt = 0
@@ -79,8 +87,8 @@ def plot(
                     if i not in rmFitDistr:
                         intMax = max(data.distr_ax[1]/((trapz(data.distr_ax[1], x=data.distr_ax[0])/len(data.distr_ax[1]))))
                         maxDistrInt = intMax if intMax>maxDistrInt else maxDistrInt
-                except:
-                    print "I could not find the file: ",val," ignoring this entry ..."
+                except Exception as e:
+                    print "I could not find the file: ",val," ignoring this entry ... ", e
                     continue
         if simFiles:
             for i, val in enumerate(simFiles):
@@ -88,8 +96,8 @@ def plot(
                     data = fn(val, simFolder, dataType='sim')
                     intMax = max(data.distr_ax[1]/((trapz(data.distr_ax[1], x=data.distr_ax[0])/len(data.distr_ax[1]))))
                     maxDistrInt = intMax if intMax>maxDistrInt else maxDistrInt
-                except:
-                    print "I could not find the file: ",val," ignoring this entry ..."
+                except Exception as e:
+                    print "I could not find the file: ",val," ignoring this entry ... ", e
                     continue
     if plotType == '4plots':
         fig, ((ax0, ax1), (ax2, ax3)) = plt.subplots(nrows=2, ncols=2)
@@ -110,16 +118,22 @@ def plot(
                 print "I could not find the file: ",val," ignoring this entry ..."
                 continue
             offset = offsetArr[i] if len(offsetArr)>=len(filesArr) else i*.1
-            colMain = deerColors[i] if len(deerColors)==len(filesArr) else defColorArr[i]
+            colMain = deerColors[i] if len(deerColors)==len(filesArr) else next(defCol)
             colOffset = backgroundColors[i] if len(backgroundColors)==len(filesArr) else ('black' if colMain != 'black' else 'gray')
             mLineWidth = mLineWidthArr[i] if len(mLineWidthArr)==len(filesArr) else 4
             oLineWidth = oLineWidthArr[i] if len(oLineWidthArr)==len(filesArr) else 2.5
             title = titlesArr[i] if len(titlesArr)==len(filesArr) else data.title.replace('_',' ')
             distrOff = deerDistOffset[i]*maxDistrInt if len(deerDistOffset)==len(filesArr) else 0
-            min_x = min(data.bckg_ax[0])
-            min_y = min(min(data.bckg_ax[1]-float(offset)),min(data.bckg_ax[2]-float(offset)))
-            max_x = max(data.bckg_ax[0])
-            max_y = max(max(data.bckg_ax[1]),max(data.bckg_ax[2]))
+            if len(timeTraceXlim) == 2:
+                min_x = min(data.bckg_ax[0][logical_and(data.bckg_ax[0]<timeTraceXlim[1], data.bckg_ax[0]>timeTraceXlim[0])])
+                min_y = min(min(data.bckg_ax[1][logical_and(data.bckg_ax[0]<timeTraceXlim[1], data.bckg_ax[0]>timeTraceXlim[0])]-float(offset)),min(data.bckg_ax[2][logical_and(data.bckg_ax[0]<timeTraceXlim[1], data.bckg_ax[0]>timeTraceXlim[0])]-float(offset)))
+                max_x = max(data.bckg_ax[0][logical_and(data.bckg_ax[0]<timeTraceXlim[1], data.bckg_ax[0]>timeTraceXlim[0])])
+                max_y = max(max(data.bckg_ax[1][logical_and(data.bckg_ax[0]<timeTraceXlim[1], data.bckg_ax[0]>timeTraceXlim[0])]),max(data.bckg_ax[2][logical_and(data.bckg_ax[0]<timeTraceXlim[1], data.bckg_ax[0]>timeTraceXlim[0])]))
+            else:
+                min_x = min(data.bckg_ax[0])
+                min_y = min(min(data.bckg_ax[1]-float(offset)),min(data.bckg_ax[2]-float(offset)))
+                max_x = max(data.bckg_ax[0])
+                max_y = max(max(data.bckg_ax[1]),max(data.bckg_ax[2]))
             ax0.plot(data.bckg_ax[0],data.bckg_ax[1]-offset, label=title, color="%s" % colMain, linewidth=mLineWidth)
             ax0.plot(data.bckg_ax[0],data.bckg_ax[2]-offset, label=None, color="%s" % colOffset, linewidth=oLineWidth)
             ax0.set_ylabel('V(t)/V(0)', weight='bold')
@@ -128,22 +142,31 @@ def plot(
             ax0.set_ylim(min_y-.02,max_y+.02)
             ax0.set_title('DEER trace + fitted background')
             leg = ax0.legend(loc='upper right', ncol=1, fontsize=8, frameon=True)
+            min_y=max_y=0.9
+            min_x=0.1
+            max_=0.6
+            if len(fitXlim) == 2:
+                min_x = min(data.fit_ax[0][logical_and(data.fit_ax[0]<fitXlim[1], data.fit_ax[0]>fitXlim[0])])
+                min_y = min(min(data.fit_ax[1][logical_and(data.fit_ax[0]<fitXlim[1], data.fit_ax[0]>fitXlim[0])]-float(offset)),min(data.fit_ax[2][logical_and(data.fit_ax[0]<fitXlim[1], data.fit_ax[0]>fitXlim[0])]-float(offset)))
+                max_x = max(data.fit_ax[0][logical_and(data.fit_ax[0]<fitXlim[1], data.fit_ax[0]>fitXlim[0])])
+                max_y = max(max(data.fit_ax[1][logical_and(data.fit_ax[0]<fitXlim[1], data.fit_ax[0]>fitXlim[0])]),max(data.fit_ax[2][logical_and(data.fit_ax[0]<fitXlim[1], data.fit_ax[0]>fitXlim[0])]))
+            min_x = min(min_x,min(data.fit_ax[0]))
+            min_y = min(min_y,min(data.fit_ax[1]-offset),min(data.fit_ax[2]-offset))
+            max_x = max(max_x,max(data.fit_ax[0]))
+            max_y = max(max_y,max(data.fit_ax[1]),max(data.fit_ax[2]))
+            ax1.plot(data.fit_ax[0],data.fit_ax[1]-offset, label=title, color="%s" % colMain, linewidth=mLineWidth)
             if i not in rmFitDistr:
-                min_y=max_y=0.9
-                min_x=0.1
-                max_=0.6
-                min_x = min(min_x,min(data.fit_ax[0]))
-                min_y = min(min_y,min(data.fit_ax[1]-offset),min(data.fit_ax[2]-offset))
-                max_x = max(max_x,max(data.fit_ax[0]))
-                max_y = max(max_y,max(data.fit_ax[1]),max(data.fit_ax[2]))
-                ax1.plot(data.fit_ax[0],data.fit_ax[1]-offset, label=title, color="%s" % colMain, linewidth=mLineWidth)
                 ax1.plot(data.fit_ax[0],data.fit_ax[2]-offset, label=None, color="%s" % colOffset, linewidth=oLineWidth)
-                ax1.set_ylabel('F(t)/F(0)', weight='bold')
-                ax1.set_xlabel(r'time [$\mu s$]', weight='bold')
-                ax1.set_xlim(0,max_x)
-                ax1.set_ylim(min_y-.02,max_y+.02)
-                ax1.set_title('Form factor + fitted distance function')
-                ax2.plot(data.distr_ax[0],data.distr_ax[1]/((trapz(data.distr_ax[1], x=data.distr_ax[0])/len(data.distr_ax[1])))+distrOff, color="%s" % colMain, linewidth=mLineWidth)
+            ax1.set_ylabel('F(t)/F(0)', weight='bold')
+            ax1.set_xlabel(r'time [$\mu s$]', weight='bold')
+            ax1.set_xlim(0,max_x)
+            ax1.set_ylim(min_y-.02,max_y+.02)
+            ax1.set_title('Form factor + fitted distance function')
+            if i not in rmFitDistr:
+                if distanceXlim:
+                    ax2.plot(data.distr_ax[0][logical_and(data.distr_ax[0]<distanceXlim[1], data.distr_ax[0]>distanceXlim[0])],data.distr_ax[1][logical_and(data.distr_ax[0]<distanceXlim[1], data.distr_ax[0]>distanceXlim[0])]/((trapz(data.distr_ax[1][logical_and(data.distr_ax[0]<distanceXlim[1], data.distr_ax[0]>distanceXlim[0])], x=data.distr_ax[0][logical_and(data.distr_ax[0]<distanceXlim[1], data.distr_ax[0]>distanceXlim[0])])/len(data.distr_ax[1][logical_and(data.distr_ax[0]<distanceXlim[1], data.distr_ax[0]>distanceXlim[0])])))+distrOff, color="%s" % colMain, linewidth=mLineWidth)
+                else:
+                    ax2.plot(data.distr_ax[0],data.distr_ax[1]/((trapz(data.distr_ax[1], x=data.distr_ax[0])/len(data.distr_ax[1])))+distrOff, color="%s" % colMain, linewidth=mLineWidth)
                 ax2.set_ylabel('P(r)', weight='bold')
                 ax2.set_xlabel(r'distance [$nm$]', weight='bold')
                 ax2.set_title('Distance distribution normalized to area')
@@ -155,7 +178,6 @@ def plot(
                     ax3.set_xlabel(r'distance [$nm$]', weight='bold')
                     ax3.set_title('Distance distribution normalized to max.')
                     ax3.set_yticks([])
-                    if distanceXlim: ax3.set_xlim(distanceXlim[0],distanceXlim[1])
     if simFiles:
         for i, val in enumerate(simFiles):
             try:
@@ -175,7 +197,19 @@ def plot(
     #fig.set_size_inches(30, fig.get_figheight(), forward=True)
     #bbox = fig.get_window_extent().transformed(fig.dpi_scale_trans.inverted())
     #width, height, dpi = bbox.width*fig.dpi, bbox.height*fig.dpi, fig.dpi
-    fig.savefig(os.path.join(os.path.dirname(plotFolder),time.strftime("%Y%m%d_")+suptitle+"PLOT.png"))
-    fig.savefig(os.path.join(os.path.dirname(plotFolder),time.strftime("%Y%m%d_")+suptitle+"PLOT.pdf"))
-    fig.savefig(os.path.join(os.path.dirname(plotFolder),time.strftime("%Y%m%d_")+suptitle+"PLOT.eps"))
-    plt.show(block=True)
+    if plotExts:
+        #print plotFolder
+        for val in plotExts:
+            figName = os.path.join(os.path.dirname(plotFolder),os.path.basename(plotFolder),time.strftime("%Y%m%d-")+suptitle.replace(' ','-')+"-PLOT."+val) if dateInPlot else os.path.join(os.path.dirname(plotFolder),os.path.basename(plotFolder),suptitle.replace(' ','-')+"-PLOT."+val)
+            try:
+                fig.savefig(figName, bbox_inches=0, transparent=True)
+                #print "Figure saved: ",figName
+            except Exception as e:
+                print e
+    if showPlot:
+        plt.show(block=True)
+        plt.close("all")
+    else:
+        plt.close("all")
+    return figName[:-4]
+
